5d4d6b5762024c19d8f8646eac072515890587e0
Franklin Otero <franklin.otero6@gmail.com>
2026-02-04 20:24:36 -0600

    Clean Architecture - Repository Pattern

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,13 @@
 import { CronJob } from "cron";
 import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository,impl";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource,
+);
 
 
 export class Server {
@@ -14,6 +21,7 @@ export class Server {
             () => {
                 const url ='https://google.com'
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(` ${ url } is ok`),
                     ( error ) => console.log( error )
                 ).execute( url );
